f48b09a65c853c841609854f4a41932aaeddcb32
MINOLHIMANSHA <it24103207@my.sliit.lk>
2025-09-25 14:25:26 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103207_LAB07.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\user\\Desktop\\IT24102008_Lab07")
+#Q1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#Q2
+pexp(2, rate = 1/3)
+
+#Q3
+1 - pnorm(130, mean = 100, sd = 15)
+
+#Q4
+qnorm(0.95, mean=100,sd=15)
